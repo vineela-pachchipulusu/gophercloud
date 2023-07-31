137664ed2caf69f3120bafe85953c25395fcecef
Takamasa Takenaka <takamasa.takenaka@windriver.com>
2023-07-31 12:02:27 -0300

    Obtain Phase/Stage/Step information (#34)
    VIM/NFV interface provides Phase/Stage/Step
    for strategy.
    This change is to obtain this information.
    
    Test Plan:
    PASS: Obtain detail information in process
          through client
    
    Signed-off-by: Takamasa Takenaka <takamasa.takenaka@windriver.com>

--- starlingx/nfv/v1/systemconfigupdate/results.go
+++ starlingx/nfv/v1/systemconfigupdate/results.go
@@ -14,6 +14,48 @@ type DeleteResult struct {
 	gophercloud.ErrResult
 }
 
+type SystemConfigStep struct {
+	StartDateTime string   `json:"start-date-time,omitempty"`
+	EndDateTime   string   `json:"end-date-time,omitempty"`
+	Timeout       int      `json:"timeout,omitempty"`
+	EntityType    string   `json:"entity-type,omitempty"`
+	StepId        int      `json:"step-id,omitempty"`
+	EntityUuids   []string `json:"entity-uuids,omitempty"`
+	StepName      string   `json:"step-name,omitempty"`
+	Result        string   `json:"result,omitempty"`
+	EntityNames   []string `json:"entity-names,omitempty"`
+	Reason        string   `json:"reason,omitempty"`
+}
+
+type SystemConfigStage struct {
+	StartDateTime string             `json:"start-date-time,omitempty"`
+	EndDateTime   string             `json:"end-date-time,omitempty"`
+	StageId       int                `json:"stage-id,omitempty"`
+	Reason        string             `json:"reason,omitempty"`
+	CurrentStep   int                `json:"current-step,omitempty"`
+	Steps         []SystemConfigStep `json:"steps,omitempty"`
+	Result        string             `json:"result,omitempty"`
+	Timeout       int                `json:"timeout,omitempty"`
+	TotalSteps    int                `json:"total-steps,omitempty"`
+	Inprogress    bool               `json:"inprogress,omitempty"`
+	StageName     string             `json:"stage-name,omitempty"`
+}
+
+type SystemConfigPhase struct {
+	StartDateTime        string              `json:"start-date-time,omitempty"`
+	EndDateTime          string              `json:"end-date-time,omitempty"`
+	PhaseName            string              `json:"phase-name,omitempty"`
+	CompletionPercentage int                 `json:"completion-percentage,omitempty"`
+	TotalStages          int                 `json:"total-stages,omitempty"`
+	StopAtStage          int                 `json:"stop-at-stage,omitempty"`
+	Result               string              `json:"sresult,omitempty"`
+	Timeout              int                 `json:"timeout,omitempty"`
+	Reason               string              `json:"reason,omitempty"`
+	Inprogress           bool                `json:"inprogress,omitempty"`
+	Stages               []SystemConfigStage `json:"stages,omitempty"`
+	CurrentStage         int                 `json:"current-stage,omitempty"`
+}
+
 // SystemConfigUpdateStrategy defines the data associated to a single System Config Update Strategy instance.
 type SystemConfigUpdate struct {
 	// ID is the generated unique UUID for the System Config Update Strategy
@@ -43,6 +85,15 @@ type SystemConfigUpdate struct {
 
 	// The strictness of alarm checks.
 	State string `json:"state"`
+
+	// Build Phase
+	BuildPhase SystemConfigPhase `json:"build-phase,omitempty"`
+
+	// Apply Phase
+	ApplyPhase SystemConfigPhase `json:"apply-phase,omitempty"`
+
+	// Abort Phase
+	AbortPhase SystemConfigPhase `json:"abort-phase,omitempty"`
 }
 
 // GenerateSCUpdate takes a JSON string and converts it into a SystemConfigUpdate structure.
